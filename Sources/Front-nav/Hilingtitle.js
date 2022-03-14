119158b7f40513f6e6ad35b1ad1f8fd62ebb7169
M. Ammar Fadhila Ramadhan <82513077+usagi13@users.noreply.github.com>
2022-03-14 23:29:25 +0700

    add apk maybe

--- Sources/Front-nav/Hilingtitle.js
+++ Sources/Front-nav/Hilingtitle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, Colors, Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
 
 const Hilingtitle = () => {
